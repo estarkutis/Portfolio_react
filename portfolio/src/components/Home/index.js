981389367b91ca9a8d58031f70e43ad231bd683b
kingeris <73307426+estarkutis@users.noreply.github.com>
2022-06-14 16:22:57 +0300

    pataisymai
    buvo bandyta atrasti problema bet dar nerasta išeitis.

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import LogoTitle from '../../assets/images/logo-s.png'
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss'
 
 const Home = () => {
