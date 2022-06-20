2112903b1dcb6d0c1ba1f85c7478ff9d65c64f7c
estarkutis <estarkutis@yahoo.com>
2022-06-20 20:29:47 +0300

    padarytas puse kontaktinio puslapio
    Padarytas užklausu langas kur galim pildyti užklausas man, bet neveikia neišsiunčia laiško. ir buvo susikurta paskira: https://dashboard.emailjs.com/admin
    kur butu galima padaryti:
    https://www.emailjs.com/docs/examples/reactjs/
    Sutvarkyta stilistika bent viskas vietose ir gražiai atrodo, bet dar truksta daug pataisymu žiurėti commitus prieš tai esančius.

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Loader from 'react-loaders'
+import emailjs from '@emailjs/browser'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
 
   useEffect(() => {
     setTimeout(() => {
@@ -12,19 +14,81 @@ const Contact = () => {
     }, 3000)
   }, [])
 
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'yahoo',
+        'Kqyp3RDXBcDuIaXu-',
+        refForm.current,
+        'f4UezfjKmXq-IrKCvw6hQ'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Faled to send the message, please try again')
+        }
+      )
+  }
+
   return (
     <>
-      <div className="container contact-page"></div>
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially ambitious or
+            large projects. However, if you have other request or question,
+            don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <Loader type="pacman" />
       </div>
-      <Loader type="pacman" />
     </>
   )
 }
